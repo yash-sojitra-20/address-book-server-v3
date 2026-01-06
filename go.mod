10dc4815e15b8633cc2bb1b014e001b2571d8075
sumit3105 <gohilsumit15@gmail.com>
2026-01-06 14:37:13 +0530

    types for project defined

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module address-book-server-v3
 
 go 1.25.5
+
+require github.com/google/uuid v1.6.0
--- internal/common/types/types.go
+++ internal/common/types/types.go
@@ -1,1 +1,28 @@
 package types
+
+import "github.com/google/uuid"
+
+const (
+	DB_HOSTNAME       = "DB_HOSTNAME"
+	DB_PORT           = "DB_PORT"
+	DB_USERNAME       = "DB_USERNAME"
+	DB_PASSWORD       = "DB_PASSWORD"
+	DB_NAME           = "DB_NAME"
+
+	SMTP_USER         = "SMTP_USER"
+	SMTP_PASS         = "SMTP_PASS"
+	SMTP_HOST         = "SMTP_HOST"
+	SMTP_PORT         = "SMTP_PORT"
+	
+	SERVER_PORT       = "SERVER_PORT"
+	JWT_SECRET        = "JWT_SECRET"
+	APP_URL           = "APP_URL"
+)
+
+// var SYSTEM_USER_ID uuid.UUID = uuid.MustParse("6a1f0001-19b2-11f0-87f7-1cc10cfb5018")
+
+type UserId uuid.UUID
+
+type AddressId uuid.UUID
+
+type Ip string
